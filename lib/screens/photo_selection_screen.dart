5d6d5b5b1e5fa210f06f457c8965d06c3c49943d
zoechcaa <zoakisinskaa@gmail.com>
2022-11-18 17:49:57 +0300

    view selected image fixed

--- lib/screens/photo_selection_screen.dart
+++ lib/screens/photo_selection_screen.dart
@@ -16,28 +16,31 @@ class PhotoSelectionPage extends StatefulWidget {
 }
 
 class _PhotoSelectionPageState extends State<PhotoSelectionPage> {
-   File? _image;
+  File? _image;
 
-   Future getImage(ImageSource source) async{
-     try{
-       await requestPermission(Permission.camera);
-       final image = await ImagePicker().pickImage(source: source);
-       if(image==null){
-         return;
-       }
-       final imagePermanent = await saveFilePermanently(image.path);
-       setState(() {
-         _image=imagePermanent;
-         log(_image!.path);
-       });
-     } on PlatformException catch(ex){
-       log('Failed to load image: $ex');
-     }
-   }
+  Future getImage(ImageSource source) async {
+    try {
+      await requestPermission(Permission.camera);
+      final image = await ImagePicker().pickImage(source: source);
+      if (image == null) {
+        return;
+      }
+      final imagePermanent = await saveFilePermanently(image.path);
+      setState(() {
+        _image = imagePermanent;
+        log(_image!.path);
+      });
+    } on PlatformException catch (ex) {
+      log('Failed to load image: $ex');
+    }
+  }
 
   Future<File> saveFilePermanently(String imagePath) async {
     final directory = await getApplicationDocumentsDirectory();
-    final time = DateTime.now().toIso8601String().replaceAll('.', '-').replaceAll(':', '-');
+    final time = DateTime.now()
+        .toIso8601String()
+        .replaceAll('.', '-')
+        .replaceAll(':', '-');
     final name = "photo_$time";
     final image = File("${directory.path}/$name");
     return File(imagePath).copy(image.path);
@@ -50,7 +53,8 @@ class _PhotoSelectionPageState extends State<PhotoSelectionPage> {
         elevation: 0,
         backgroundColor: Colors.transparent,
         automaticallyImplyLeading: false,
-        title: Text(appBarSelectingPage, style: Theme.of(context).textTheme.headline6),
+        title: Text(appBarSelectingPage,
+            style: Theme.of(context).textTheme.headline6),
       ),
       body: Center(
         child: Column(
@@ -59,41 +63,41 @@ class _PhotoSelectionPageState extends State<PhotoSelectionPage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 IconButton(
-                  onPressed: () {
-                     getImage(ImageSource.camera);
-                  },
+                  onPressed: () => getImage(ImageSource.camera),
                   icon: const Icon(
                     Icons.camera_alt_outlined,
                     size: 50,
                   ),
                 ),
-                const SizedBox(
-                  width: 40,
-                ),
+                const SizedBox(width: 40,),
                 IconButton(
-                  onPressed: () {
-                    getImage(ImageSource.gallery);
-                  },
-                  icon: const Icon(
-                    Icons.image_outlined,
-                    size: 50,
-                  ),
+                  onPressed: () => getImage(ImageSource.gallery),
+                  icon: const Icon(Icons.image_outlined, size: 50,),
                 ),
               ],
             ),
-           const SizedBox(height: 20,),
-           if(_image!=null) ElevatedButton(
-             style: ElevatedButton.styleFrom(
-               backgroundColor: Theme.of(context).primaryColor,
-               shape: RoundedRectangleBorder(
-                 borderRadius: BorderRadius.circular(16),
-               ),
-             ),
-               onPressed: ()=> Navigator.pushNamed(context, editPhotoPage, arguments: _image),
-               child: Text('Edit this photo', style: Theme.of(context).textTheme.subtitle1,),
-           ),
-           const SizedBox(height: 20,),
-           if(_image!=null) Image.file(_image!, fit: BoxFit.cover, width: 300,),
+            const SizedBox(height: 20,),
+            if (_image != null)
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(context).primaryColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+                onPressed: () => Navigator.pushNamed(context, editPhotoPage, arguments: _image),
+                child: Text('Edit this photo', style: Theme.of(context).textTheme.subtitle1,
+                ),
+              ),
+            const SizedBox(height: 20,),
+            if (_image != null)
+              ConstrainedBox(
+                constraints: BoxConstraints(
+                  maxHeight: 0.6 * MediaQuery.of(context).size.height,
+                  maxWidth: 0.8 * MediaQuery.of(context).size.width,
+                ),
+                child: Image.file(_image!,),
+              ),
           ],
         ),
       ),
